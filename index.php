25e11bcfac5a7210675db79e8be83921f5e1dead
FisterMedister <74867161+FisterMedister@users.noreply.github.com>
2022-05-25 12:15:33 +0200

    update

--- index.php
+++ index.php
@@ -2,16 +2,41 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lifeline Robotics - For investors</title>
+    <link rel="stylesheet" href="src/css/style.css">
 </head>
 <body>
     <header>
 
-    
+
 
     </header>  
+
+    <section id="fsection">
+    <h1>For Investors</h1>
+
+    <div class="fsection-grid">
+
+    <div class="grid-box1">
+
+    </div>
+
+    <div class="grid-box2">
+
+    </div>
+
+    <div class="grid-box3">
+
+    </div>
+    
+    <div class="grid-box4">
+
+    </div>
+
+    </div><!-- First Section Grid -->
+    
+    </section>
 
     <footer>
 
